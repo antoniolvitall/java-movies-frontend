242eb33e8d250536f9814a356766494093ffc6a2
Antonio Lindo <antoniolvitall@gmail.com>
2023-07-29 12:10:46 -0300

    Problema com axios resolvido e iniciando criacao de componentes

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://44b8-2804-14d-5c80-91e3-80cc-54b4-fb87-968a.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL:'http://localhost:8080'
 });
