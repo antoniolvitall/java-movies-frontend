6aab668176a214c3b3bcedb983c422984af3f5f1
Antonio Lindo <antoniolvitall@gmail.com>
2023-07-29 20:02:08 -0300

    bug corrigido

--- movie-gold-v1/src/index.js
+++ movie-gold-v1/src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-          <Route path="/*" elemente={<App />} />
+          <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
