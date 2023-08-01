328dcd76945fe49524068ff7ccf157d7d00932e7
Antonio Lindo <antoniolvitall@gmail.com>
2023-08-01 19:51:42 -0300

    finalizando componente trailer

--- movie-gold-v1/src/components/hero/Hero.js
+++ movie-gold-v1/src/components/hero/Hero.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Hero = ({movies}) => {
   return (
@@ -19,6 +22,15 @@ const Hero = ({movies}) => {
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movies.title}</h4>
+                                        </div>
+                                        <div className="movie-buttons-contaienr">
+                                            <Link to={`/Trailer/${movies.trailerLink.substring(movies.trailerLink.length - 11)}`}>
+                                            <div className="play-button-icon-container">
+                                                <FontAwesomeIcon className="play-button-icon"
+                                                    icon={faCirclePlay}
+                                                />
+                                            </div>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
